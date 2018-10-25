b32e089877dd8629e58c1ca7b38603e291bb697d
yidafu <1427019344@qq.com>
2018-10-25 15:24:43 +0800

    feat(app/controller/Seech.js): resouce speeches index methods implement.

--- packages/backend/app/controller/Speech.js
+++ packages/backend/app/controller/Speech.js
@@ -1,11 +1,43 @@
-const {Controller} = require('egg')
+const {
+  Controller
+} = require('egg')
+const dateFormat = require('dateformat')
+
 class SpeechController extends Controller {
   async index() {
     const {
       ctx
     } = this
-    ctx.body  = await ctx.model.Speech.findAll()
-    // ctx.respond.header('Access-Control-Allow-Origin', '*')
+    const speeches = await ctx.model.Speech.findAll()
+
+    ctx.body = speeches.map(speech => {
+      const {
+        id,
+        direction,
+        speaker_name,
+        isPPT,
+        url,
+        subject,
+        introduce,
+        pre_knowledge,
+        tags,
+        created_at,
+        speech_date,
+      } = speech
+      return {
+        tags: tags.split(','),
+        speech_date: dateFormat(speech_date, 'fullDate'),
+        created_date: dateFormat(created_at, 'fullDate'),
+        id,
+        direction,
+        speaker_name,
+        isPPT,
+        url,
+        subject,
+        introduce,
+        pre_knowledge,
+      }
+    })
   }
 }
 module.exports = SpeechController
