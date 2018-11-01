6d0077e84dca9b05a5daef014a82148166074950
yidafu <1427019344@qq.com>
2018-11-01 23:52:06 +0800

    refactor(add a attributes count.):

--- packages/backend/app/serializer/labelSerializer.js
+++ packages/backend/app/serializer/labelSerializer.js
@@ -1,5 +1,5 @@
 const {Serializer} = require('jsonapi-serializer')
 module.exports = new Serializer('tag',{
   keyForAttribute: 'underscore_case',
-  attributes: ['value']
+  attributes: ['value','count']
 })
