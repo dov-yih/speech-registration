7d3c9ab623a97b55230d328227564630499e738f
yidafu <1427019344@qq.com>
2018-11-01 23:47:21 +0800

    feat(src/componets/speech/MultipleSelect.jsx): Powerful multiple select by using rc-select.
    #5

---
+++ packages/frontend/src/components/speech/MultipleSelect.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import Select from 'rc-select'
+import orderBy from 'lodash/orderBy'
+import Tag from '@/network/tag'
+
+import 'rc-select/assets/index.css'
+
+export default class MultipleSelect extends Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }
+  static defaultProps = {
+    type: 'direction'
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataSet: []
+    }
+  }
+  async componentDidMount() {
+    let data = await Tag.get();
+    let dataSet = orderBy(data.data, ['count'], ['desc'])
+    this.setState({ dataSet })
+  }
+  render() {
+    const { onChange, label } = this.props
+    const { dataSet } = this.state
+    return <div>
+      <div>
+        <label>{label}:</label>
+        <Select
+          style={{ width: '100%' }}
+          multiple
+          onChange={onChange}
+          animation='slide-up'
+          allowClear
+          tokenSeparators={[' ', ',']}
+        >
+          {dataSet.map((data, idx) => (
+            <option key={idx} value={data.value}>{data.value}</option>
+          ))}
+        </Select>
+      </div>
+    </div>
+  }
+}
